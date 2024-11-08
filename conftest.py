8338d10b5184099ee4b0e7b422d752717a6ff6a4
v.parfenova <vestenok1984@mail.ru>
2024-11-08 10:54:01 +0300

    Домашняя работа2

--- conftest.py
+++ conftest.py
@@ -9,16 +9,16 @@ path_to_directory_files = os.path.join(current_dir, "files")
 path_to_directory_archive = os.path.join(current_dir, "arhives")
 
 
-@pytest.fixture(scope="session", autouse=False)
+@pytest.fixture(scope="function", autouse=False)
 def add_files_to_zip():
     if not os.path.exists(path_to_directory_archive):
         os.mkdir(path_to_directory_archive)
-    elif not os.path.exists(os.path.join(path_to_directory_archive, "test.zip")):
-        file_dir = os.listdir(path_to_directory_files)
-        with zipfile.ZipFile(os.path.join(path_to_directory_archive, "test.zip"), mode="w") as zf:
-            for file in file_dir:
-                add_file = os.path.join(path_to_directory_files, file)
-                zf.write(add_file, os.path.basename(add_file))
+
+    file_dir = os.listdir(path_to_directory_files)
+    with zipfile.ZipFile(os.path.join(path_to_directory_archive, "test.zip"), mode="w") as zf:
+        for file in file_dir:
+            add_file = os.path.join(path_to_directory_files, file)
+            zf.write(add_file, os.path.basename(add_file))
 
     yield
 
